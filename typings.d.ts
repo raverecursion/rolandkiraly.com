64d069962a9447e029d621bb184b75f6175c3f72
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-08-23 15:21:04 +0200

    removing redundant LinkCard component

--- typings.d.ts
+++ typings.d.ts
@@ -1,4 +1,5 @@
 declare module '@mapbox/rehype-prism';
 declare module '@next/mdx';
 declare module 'remark-code-titles';
-declare module '@chakra-ui/react';
+// declare module '@chakra-ui/react';
+declare module 'HeadingProps';
