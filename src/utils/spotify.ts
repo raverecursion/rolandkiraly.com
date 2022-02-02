2cfbdd0962dc81b35242c349f52a94edae97704e
innellea <rolandkiraly94@gmail.com>
2022-02-02 22:42:00 +0000

    changing spotify refresh token

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -2,7 +2,7 @@ import querystring from 'querystring';
 
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN_MAIN;
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString(`base64`);
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
